ebe1e9e8414fb744519e334904f19e8479b6f571
Nilisha Maharjan <nili.mhj@gmail.com>
2025-10-15 10:11:42 +0545

    Removed unused variable 'box' in main.dart

--- lib/util/dialog_box.dart
+++ lib/util/dialog_box.dart
@@ -3,8 +3,8 @@ import 'button.dart';
 
 class DialogBox extends StatelessWidget{
   final controller;
-  VoidCallback onSave;
-  VoidCallback onCancel;
+  final VoidCallback onSave;
+  final VoidCallback onCancel;
   DialogBox({
     super.key, 
     required this.controller,
